5e5ea6ee41fdcb1ab83fb9df30100dd8ad03c158
qkhuyit <qkhuyit@gmail.com>
2017-06-21 00:26:15 +0700

    fix tree

--- Public/more/tree/renxi/love.js
+++ Public/more/tree/renxi/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-            ctx.fillText("Vy Nghĩa", 23, 10);
+            ctx.fillText("Bích Diễm", 23, 10);
             ctx.restore();
         },
         clear: function() {
