96859a750b523a7e2dedf26d9c8d28babe339077
qkhuyit <qkhuyit@gmail.com>
2017-06-25 16:44:42 +0700

    qưeqweqweq

--- App/Controllers/CanvasController.js
+++ App/Controllers/CanvasController.js
@@ -6,5 +6,9 @@ router.get('/hung-y',(req,res) => {
     res.render('More/HungLe',{layout : false});
 });
 
+router.get('/tang-ban',(req,res) => {
+    res.render('More/HungLe2',{layout : false});
+});
+
 
 module.exports = router;
