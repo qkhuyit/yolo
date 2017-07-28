5b9b6f1980cf4a41a1722b21516fcbc687310256
QKHUYIT\qkhuyIT <qkhuyit@gmail.com>
2017-07-28 22:51:31 +0700

    sdfds

--- App/Controllers/CanvasController.js
+++ App/Controllers/CanvasController.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router = express.Router();
 
 
-router.get('/hung-y',(req,res) => {
+router.get('/love-thuyen-forever',(req,res) => {
     res.render('More/HungLe',{layout : false});
 });
 
